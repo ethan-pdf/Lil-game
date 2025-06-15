4f7b0ced03366f87db2dd31a8792c797f5352555
Ethan Benjamin <144292396+ethan-pdf@users.noreply.github.com>
2025-06-14 22:45:28 -0400

    added some mechanics

---
+++ Lil Game Project/Assets/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Health;
+    public int Damage;
+
+
+
+    void Start()
+    {
+        Health = 10;
+        Damage = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
